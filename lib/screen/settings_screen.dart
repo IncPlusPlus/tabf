eac7445fff83696073e604728ed70c79a7c08e5e
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-07-24 14:15:30 -0400

    Add login functionality and necessary stub screens

---
+++ lib/screen/settings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+/// Settings screen.
+class SettingsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
